e6c0335ab899ccc5a5fce3e63d44de76413d29d7
lsw1232414 <limsw980821@gmail.com>
2025-09-05 17:35:04 +0900

    0905

---
+++ day0904/src/chap5/chap5_sec04.java
@@ -0,0 +1,15 @@
+package chap5;
+
+public class chap5_sec04 {
+
+	public static void main(String[] args) {
+		String hobby = "여행";
+		hobby = null;
+		
+		String kind1 = "자동차";
+		String kind2 = kind1;
+		kind1 = null;
+		System.out.println("kind2: "+ kind2);
+	}
+
+}
---
+++ day0904/src/chap5/chap5_sec05.java
@@ -0,0 +1,10 @@
+package chap5;
+
+public class chap5_sec05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
