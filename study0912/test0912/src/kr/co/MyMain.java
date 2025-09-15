e74b202f06ad683d144cfaf8b0c427c7576b74a7
lsw1232414 <limsw980821@gmail.com>
2025-09-15 15:36:43 +0900

    0915

---
+++ study0912/test0912/src/kr/co/MyMain.java
@@ -0,0 +1,10 @@
+package kr.co;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ study0912/test0912/src/kr/co/ext/ExtClass.java
@@ -0,0 +1,5 @@
+package kr.co.ext;
+
+public class ExtClass {
+
+}
